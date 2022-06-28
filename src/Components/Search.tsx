d29ab22be8c18b62e957e7e7e07074b094db9b0f
michael baumgarten <michael.baumgarn@gmail.com>
2022-06-28 16:21:34 +0200

    add key enter for search

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -9,8 +9,23 @@ export const Search = ({ onSubmit }: SearchProps) => {
   const [search, setSearch] = useState("");
   return (
     <Box my={8}>
-      <Input onChange={(e) => setSearch(e.target.value)} value={search}></Input>
-      <Button my={4} onClick={() => onSubmit(search)}>
+      <Input
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onSubmit(search);
+            setSearch("");
+          }
+        }}
+      ></Input>
+      <Button
+        my={4}
+        onClick={() => {
+          onSubmit(search);
+          setSearch("");
+        }}
+      >
         Search for {search}
       </Button>
     </Box>
